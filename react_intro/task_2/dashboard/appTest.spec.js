c3479181b561b7ba8b6d37165c87f4abb558f6c4
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-18 16:25:18 +0200

    error fix

--- react_intro/task_2/dashboard/appTest.spec.js
+++ react_intro/task_2/dashboard/appTest.spec.js
@@ -1,6 +1,6 @@
-const { getCurrentYear } = require('./src/utils');
+// adjust this path depending on where utils.js really lives
+const { getCurrentYear } = require('../src/utils');
 
-// Manual check for getCurrentYear
 const year = new Date().getFullYear();
 if (getCurrentYear() !== year) {
   throw new Error("getCurrentYear did not return the current year");
