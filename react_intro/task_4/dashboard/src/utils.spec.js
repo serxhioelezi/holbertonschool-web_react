2c1f51afd3220f91d61ad67a85f7968119df74b8
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-22 17:30:39 +0200

    task 4 and 5

--- react_intro/task_4/dashboard/src/utils.spec.js
+++ react_intro/task_4/dashboard/src/utils.spec.js
@@ -1,17 +1,24 @@
-export const getCurrentYear = () => {
-    const dateTime = new Date();
-    return dateTime.getFullYear();
-}
-export const getFooterCopy = (isIndex) => {
-    if (isIndex) {
-        return "Holberton School";
-    } else {
-        return "Holberton School main dashboard";
-    }
-}
+import { getCurrentYear, getFooterCopy, getLatestNotification } from "../../../task_2/dashboard/src/utils.jsx";
 
-export const getLatestNotification = () => {
-    return "<strong>Urgent requirement</strong> - complete by EOD";
-}
+describe("Utils function", () => {
+    test("getCurrentYear return the current year", () => {
+        const year = getCurrentYear();
+        const expectedYear = new Date().getFullYear();
+        expect(year).toBe(expectedYear);
+    });
 
-export default { getCurrentYear, getFooterCopy }
+    test("getFooterCopy() returns 'Holberton School' when true", () => {
+        expect(getFooterCopy(true)).toBe("Holberton School");
+    });
+
+    test("getFooterCopy() returns 'Holberton School main dashboard' when false", () => {
+        expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+    });
+
+    test("getLatestNotification returns the correct string", () => {
+        expect(getLatestNotification()).toBe(
+            "<strong>Urgent requirement</strong> - complete by EOD"
+        );
+    });
+
+});
